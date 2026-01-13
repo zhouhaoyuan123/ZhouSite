5e17a8316b6e8970e910273f654edc9961a9e784
zhouhaoyuan123 <95029044+zhouhaoyuan123@users.noreply.github.com>
2026-01-13 11:25:28 +0000

    add license

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ mkdir -p dist
 rm -rf dist/*
 
 # List of files and directories to exclude
-exclude=".config .git attached_assets .upm .replit build.sh README.md replit.md dist build_search.sh"
+exclude=".config .git attached_assets .upm .replit build.sh README.md replit.md dist build_search.sh LICENSE"
 
 # Move all files and folders to the dist directory, excluding the specified ones
 for item in *; do
